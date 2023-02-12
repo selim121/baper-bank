7a31aea38d2fa0b0a410fd6fda5a04fc9e133343
selim121 <selimkhan.bdkhan@gmail.com>
2023-02-12 15:19:44 +0600

    another page added

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
 document.getElementById('btn-submit').addEventListener('click', function(){
-    
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    if(email === 'son@baap.com' && password === 'secret'){
+        window.location.href = 'bank.html';
+    }else{
+        alert('Invalid user!');
+    }
 })
